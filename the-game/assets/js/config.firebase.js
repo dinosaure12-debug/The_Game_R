276cf89e2bbdc93eab63100d000344c0eae2a8ca
dinosaure12-debug <dinosaure12@gmail.com>
2025-12-23 14:21:09 +0100

    Delete the-game directory

--- the-game/assets/js/config.firebase.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCRRVuWD29vt6qgp1iEBfPha-mNRQO2C54",
-  authDomain: "the-game-80e93.firebaseapp.com",
-  projectId: "the-game-80e93",
-  storageBucket: "the-game-80e93.firebasestorage.app",
-  messagingSenderId: "147107639100",
-  appId: "1:147107639100:web:448b58c01837884bd33839"
-};
